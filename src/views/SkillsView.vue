8f3b75a882e4c7d7a79ad46f2ca566aa976effd3
an80546 <a0922776220@gmail.com>
2026-05-28 16:33:20 +0800

    bug update

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -9,8 +9,9 @@ import { skills } from '../data/skills'
       <p class="eyebrow">Skills</p>
       <h1 class="page-title">技能頁</h1>
       <p class="page-description">
-      <p>這裡使用 v-for 渲染技能資料，並透過 props 將每一筆技能傳入 SkillCard 元件。</p>
-      <p>擁有的證照以及擅長的技能、領域</p>
+      <span>這裡使用 v-for 渲染技能資料，並透過 props 將每一筆技能傳入 SkillCard 元件。</span>
+      <br>
+      <span>擁有的證照以及擅長的技能、領域</span>
       </p>
     </header>
 
